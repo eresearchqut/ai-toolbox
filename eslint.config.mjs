e50402b826865d7105173d8026bdd93fe5f1f4c1
ScriptSmith <ScriptSmith@users.noreply.github.com>
2024-07-02 17:06:57 +1000

    Add eslint, prettier with config for JSX support and import ordering

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
+export default [
+  { ignores: [".docusaurus/", "build/", "docs/", "blog/"] },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  {
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node, Intl: true },
+    },
+  },
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  pluginReactConfig,
+  {
+    rules: {
+      "react/react-in-jsx-scope": 0,
+      "react/jsx-uses-react": 0,
+      "react/prop-types": 0,
+    },
+  },
+  {
+    rules: {
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    },
+  },
+];
