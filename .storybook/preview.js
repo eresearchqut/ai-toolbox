807f6c01d35eb38d98948e72c4fc630e7461b858
ScriptSmith <ScriptSmith@users.noreply.github.com>
2024-08-05 14:42:50 +1000

    Upgrade to storybook 8.2

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,7 +3,6 @@ import { toolboxTheme } from "../src/theme/Root";
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
