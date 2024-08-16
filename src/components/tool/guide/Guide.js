4da554f9c0f29bf5062436551f42238f43b09b19
ScriptSmith <ScriptSmith@users.noreply.github.com>
2024-08-16 16:15:13 +1000

    Don't wrap description with link

--- src/components/tool/guide/Guide.js
+++ src/components/tool/guide/Guide.js
@@ -11,30 +11,32 @@ import { createContext, useContext } from "react";
 
 export function GuideHeader({ title, subtitle, logo, alt, link }) {
   return (
-    <Link href={link} isExternal>
-      <Image
-        float="left"
-        verticalAlign="middle"
-        src={logo}
-        alt={alt}
-        display="inline"
-        height="30px"
-      />
-      <Heading
-        as="h1"
-        size="lg"
-        display="inline"
-        position="relative"
-        marginLeft="2"
-      >
-        {title}
-      </Heading>
+    <>
+      <Link href={link} isExternal>
+        <Image
+          float="left"
+          verticalAlign="middle"
+          src={logo}
+          alt={alt}
+          display="inline"
+          height="30px"
+        />
+        <Heading
+          as="h1"
+          size="lg"
+          display="inline"
+          position="relative"
+          marginLeft="2"
+        >
+          {title}
+        </Heading>
+      </Link>
       {subtitle && (
         <Heading as="h3" size="xs" mt={4}>
           {subtitle}
         </Heading>
       )}
-    </Link>
+    </>
   );
 }
 
